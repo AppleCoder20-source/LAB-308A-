f898ac837b1a686f31a31f2b9545762d458a2db9
Mashiur <56059603+AppleCoder20-source@users.noreply.github.com>
2024-10-24 00:02:38 -0400

    Finished Lab

---
+++ index.js
@@ -0,0 +1,50 @@
+let counter = 0;
+
+function recursive() {
+    counter +=1
+    recursive()
+}
+try{
+    recursive()
+} catch(error){
+    console.log("Error caught:", error.message);
+    console.log("Counter value when error occurred:", counter);
+}
+//Part 2
+function flattenRecursive(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        let elements = array[i];
+        if (Array.isArray(elements)) {
+            result = result.concat(flattenRecursive(elements));
+        } else {
+            result.push(elements);
+        }
+    }
+    return result;
+}
+
+// Trampoline Function
+function trampoline(func) {
+    return function (...args) {
+        let result = func(...args);
+        while (typeof result === 'function') {
+            result = result();
+        }
+        return result;
+    };
+}
+//Test both flatten array and with trampoline
+const flat = flattenRecursive([1, [2, [3, [4, [5]]]]],)
+console.log(flat)
+
+const Tramp = trampoline(flattenRecursive)
+
+Tramp([
+    [1, 2, [3, 4], 5],
+    [1, [2, [3, [4, [5]]]]],
+    [1, [2, 3], [[4, 5], [6]], 7],
+    [1, [], [2, [3, []]], 4],
+    [1, 'a', [true, [null, [undefined]]], 2],
+]);
+
